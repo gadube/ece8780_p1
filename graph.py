d8a447128f4df6f1246607e739edc59c70d089c6
Griffin <gdube@g.clemson.edu>
2021-02-03 23:47:29 -0500

    Updating readme, adding report and project guide

--- graph.py
+++ graph.py
@@ -10,7 +10,7 @@ kernel = kernel[kernel['Name'] != 'CUDA memcpy DtoH']
 dtoh = df[df['Name'] == 'CUDA memcpy DtoH']
 htod = df[df['Name'] == 'CUDA memcpy HtoD']
 transfers = df[df['Name'] != 'im2Gray']
-print(df.head(60))
+print(kernel)
 
 transfers['Size (B)'] = transfers['Size (B)'].astype(float)
 transfers['Size (B)'] = transfers['Size (B)'] / 2**20
@@ -22,33 +22,39 @@ kernel['Duration'] = kernel['Duration'] * 10**9
 
 
 #graphing image sizes
-g = sns.catplot(x="Input Image",y="Size (B)",kind="bar",hue="Name",aspect=1.3,ci=None,data=transfers)
+g = sns.catplot(x="Input Image",y="Size (B)",kind="bar",hue="Name",aspect=1.4,ci=None,data=transfers)
 g.set_xticklabels(["Baboon\n(512x512)","Dog\n(1600x1200)","Lemons\n(3456x2304)","Lena\n(400x225)","Peppers\n(506x326)"])
 #g.set_xticklabels(["Baboon\n(512x512)","Dog\n(1600x1200)","Lena\n(400x225)","Peppers\n(506x326)"])
 plt.ylabel("Size (MB)")
 plt.gca().set_yscale("log",base=2)
 g.savefig('imagesizes.png')
 
-g = sns.catplot(x="Input Image",y="Throughput (GB/s)",row="Device Name",kind="bar",hue="Name",aspect=1.3,ci=None,data=transfers)
+g = sns.catplot(x="Input Image",y="Throughput (GB/s)",row="Device Name",kind="bar",hue="Name",aspect=1.4,ci=None,data=transfers)
 g.set_xticklabels(["Baboon\n(512x512)","Dog\n(1600x1200)","Lemons\n(3456x2304)","Lena\n(400x225)","Peppers\n(506x326)"])
 #g.set_xticklabels(["Baboon\n(512x512)","Dog\n(1600x1200)","Lena\n(400x225)","Peppers\n(506x326)"])
 axes = g.axes.flatten()
 axes[0].set_ylabel("Throughput (GB/s) - Higher is Better")
+axes[1].set_ylabel("Throughput (GB/s) - Higher is Better")
+axes[2].set_ylabel("Throughput (GB/s) - Higher is Better")
 g.savefig('imagethroughput.png')
 
-g = sns.catplot(x="Input Image",y="Duration",row="Device Name",kind="bar",hue="Name",aspect=1.3,ci=None,data=transfers)
+g = sns.catplot(x="Input Image",y="Duration",row="Device Name",kind="bar",hue="Name",aspect=1.4,ci=None,data=transfers)
 g.set_xticklabels(["Baboon\n(512x512)","Dog\n(1600x1200)","Lemons\n(3456x2304)","Lena\n(400x225)","Peppers\n(506x326)"])
 #g.set_xticklabels(["Baboon\n(512x512)","Dog\n(1600x1200)","Lena\n(400x225)","Peppers\n(506x326)"])
 g.set(yscale="log")
 axes = g.axes.flatten()
 axes[0].set_ylabel("Duration (μs) - Lower is Better")
+axes[1].set_ylabel("Duration (μs) - Lower is Better")
+axes[2].set_ylabel("Duration (μs) - Lower is Better")
 g.savefig('imageduration.png')
 
 hue_order = ["['4', '4', '1']","['8', '8', '1']","['16', '16', '1']","['32', '32', '1']"]
-g = sns.catplot(x="Input Image",y="Duration",row="Device Name",kind="bar",hue="Block Size",hue_order=hue_order,aspect=1.3,ci=None,data=kernel)
+g = sns.catplot(x="Input Image",y="Duration",row="Device Name",kind="bar",hue="Block Size",hue_order=hue_order,aspect=1.4,ci=None,data=kernel)
 g.set_xticklabels(["Baboon\n(512x512)","Dog\n(1600x1200)","Lemons\n(3456x2304)","Lena\n(400x225)","Peppers\n(506x326)"])
 #g.set_xticklabels(["Baboon\n(512x512)","Dog\n(1600x1200)","Lena\n(400x225)","Peppers\n(506x326)"])
 g.set(yscale="log")
 axes = g.axes.flatten()
 axes[0].set_ylabel("Duration (μs) - Lower is Better")
+axes[1].set_ylabel("Duration (μs) - Lower is Better")
+axes[2].set_ylabel("Duration (μs) - Lower is Better")
 g.savefig('kernelduration.png')
