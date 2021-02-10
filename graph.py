caf093610a6ae79477d40795d1c02713a14b77b8
Griffin Dube <gdube@g.clemson.edu>
2021-02-10 16:48:43 -0500

    Updating graph script to graph shared memory kernel outputs

--- graph.py
+++ graph.py
@@ -3,6 +3,12 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 
+font = {'family' : 'normal',
+        'weight' : 'bold',
+        'size'   : 16}
+
+plt.rc('font', **font)
+
 df = pd.read_csv("profiling.csv")
 #df = df[df["Input Image"] != 'lemons_3456_2304.png']
 kernel = df[df['Name'] != 'CUDA memcpy HtoD']
@@ -47,13 +53,14 @@ kernel['Duration'] = kernel['Duration'] * 10**9
 #axes[2].set_ylabel("Duration (μs) - Lower is Better")
 #g.savefig('imageduration.png')
 
-hue_order = ["['4', '4', '1']","['8', '8', '1']","['16', '16', '1']","['32', '32', '1']"]
-g = sns.catplot(x="Input Image",y="Duration",row="Device Name",col="Block Size",col_order=hue_order,kind="bar",hue="Name",aspect=1.4,ci=None,data=kernel)
-g.set_xticklabels(["Baboon\n(512x512)","Dog\n(1600x1200)","Lemons\n(3456x2304)","Lena\n(400x225)","Peppers\n(506x326)"])
+hue_order = ["['4', '4', '1']","['8', '8', '1']"]
+hue_order2= ["['16', '16', '1']","['32', '32', '1']"]
+g = sns.catplot(x="Input Image",y="Duration",row="Device Name",col="Block Size",col_order=hue_order2,kind="bar",hue="Name",aspect=1.4,ci=None,data=pd.concat([kernel[kernel['Block Size'] == "['16', '16', '1']"],kernel[kernel['Block Size'] == "['32', '32', '1']"]]))
+g.set_xticklabels(["Baboon","Dog","Lemons","Lena","Peppers"])
 #g.set_xticklabels(["Baboon\n(512x512)","Dog\n(1600x1200)","Lena\n(400x225)","Peppers\n(506x326)"])
 g.set(yscale="log")
 axes = g.axes.flatten()
 axes[0].set_ylabel("Duration (μs) - Lower is Better")
-axes[1].set_ylabel("Duration (μs) - Lower is Better")
-#axes[2].set_ylabel("Duration (μs) - Lower is Better")
+axes[2].set_ylabel("Duration (μs) - Lower is Better")
+axes[4].set_ylabel("Duration (μs) - Lower is Better")
 g.savefig('kernelduration.png')
